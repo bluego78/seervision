311465b34d9249f374f74b171fe45e147e1ffbb7
Maurizio Podda <m.podda@consulcesi.com>
2021-05-01 17:51:36 +0200

    refactors some test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,13 +1,26 @@
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
-
+import { shallow } from 'enzyme';
 import App from '../components/App';
-import { store } from '../stores/TodoStore';
-import ITodoState from '../Interfaces/ITodoState';
+import { initState } from '../stores/TodoStore';
 import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 
+
+const mockStore = configureStore([]);
+let mockedStore:any;
+let component:any;
 let container:any = null;
-beforeEach(() => {
+let appState:any;
+
+beforeEach(async () => {
+  mockedStore = mockStore(initState);
+  appState = await mockedStore.getState();
+  component = shallow(
+    <Provider store={mockedStore}>
+      <App appState={appState} />
+    </Provider>
+  )
   // setup a DOM element as a render target
   container = document.createElement("div");
   document.body.appendChild(container);
@@ -21,26 +34,22 @@ afterEach(() => {
 });
 
 describe('<App /> component tests', () => {
-
+    
+    
     it("App renders the loading spinner if iLoading is true", async () => {
-        
-        var appState = await store.getState();
         appState.isLoading = true;
         act(() => {
-          render(<Provider store={store}><App appState={appState} /></Provider>, container);
+          render(<Provider store={mockedStore}><App /></Provider>, container);
         });
-
         expect(container.querySelector(".spinner")).toBeInTheDocument;
     });
+ 
 
     it("App doesn't render the loading spinner if iLoading is false", async () => {
-        
-        var appState = await store.getState();
         appState.isLoading = false;
         act(() => {
-          render(<Provider store={store}><App appState={appState} /></Provider>, container);
+          render(<Provider store={mockedStore}><App /></Provider>, container);
         });
-
         expect(container.querySelector(".spinner")).not.toBeInTheDocument;
     });
 
